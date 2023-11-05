aaceb2e96d04d0054c35bacace57e4657cf8e30a
Sara Morillon <sara.morillon.pro@gmail.com>
2023-11-05 12:06:55 +0100

    Add gitignore

--- game of life/src/canvas.ts
+++ game of life/src/canvas.ts
@@ -14,15 +14,13 @@ export function drawCell(x: number, y: number, value: boolean) {
   const rectY = y * settings.size
   const width = settings.size
   const height = settings.size
-  const radius = (settings.size - 2) / 2
   ctx.save()
-  ctx.clearRect(rectX, rectY, width, height)
   ctx.fillStyle = '#F1F3F5'
   ctx.fillRect(rectX + 1, rectY + 1, width - 2, height - 2)
   if (value) {
     ctx.fillStyle = '#303030'
     ctx.beginPath()
-    ctx.arc(rectX + radius + 1, rectY + radius + 1, radius, 0, Math.PI * 2)
+    ctx.arc(rectX + settings.size / 2, rectY + settings.size / 2, settings.size / 2 - 2, 0, Math.PI * 2)
     ctx.closePath()
     ctx.fill()
   }
